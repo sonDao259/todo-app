5aba56d793e12abd22387c48b89c835288ae3283
Dao Hoang Son <hoangson28122017@gmail.com>
2022-04-19 09:40:30 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
-
 function App() {
+  const [label, setLabel] = useState("Nothing to do")
+  const [task, setTask] = useState("")
+  const [tasks, setTasks] = useState([])
+  const inputRef = useRef()
+  const handleAdd = () => {
+    inputRef.current.focus()
+    setTask("")
+    setLabel("Task(s): ")
+    setTasks(prev => {
+      return [...prev, task]
+    })
+  }
+  const handleRemove = index => {
+    const newTasks = [...tasks]
+    newTasks.splice(index, 1)
+    console.log(tasks.length);
+    if (newTasks.length === 0) 
+      setLabel("Nothing to do")
+    return setTasks(newTasks)
+  }
+  useEffect(() => {
+    console.log(tasks);
+  }, [tasks])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Todo App</h1>
+      <div>
+        <input 
+          value = {task}
+          onChange = {(e) => setTask(e.target.value)}
+          ref = {inputRef}
+          className = "input-box"
+          placeholder = "Add task here..."
+        />
+        <button 
+          onClick={handleAdd}
+          className = "submit-btn"
         >
-          Learn React
-        </a>
-      </header>
+          +
+        </button>
+        <ol className='list'>
+          <p className="label">{label}</p>
+          {tasks.map((task, index) => (
+            <React.Fragment key = {index}>
+                <li className = "list-item">{task}</li>
+                <button 
+                  onClick={() => handleRemove(index)}
+                  className = "remove-btn"
+                > 
+                  &times;
+                </button>
+              </React.Fragment>
+          ))}
+        </ol>
+      </div>
     </div>
   );
 }
