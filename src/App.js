65ff50248fd989aaf6c33f2008e9842ebd5c01c0
Dao Hoang Son <hoangson28122017@gmail.com>
2022-04-20 14:23:42 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,63 +1,77 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
+
+const handleJSON = () => {
+  return JSON.parse(localStorage.getItem("tasks"))
+}
+
 function App() {
-  const [label, setLabel] = useState("Nothing to do")
   const [task, setTask] = useState("")
-  const [tasks, setTasks] = useState([])
+  const [tasks, setTasks] = useState(handleJSON ?? [])
+
   const inputRef = useRef()
+
   const handleAdd = () => {
     inputRef.current.focus()
     setTask("")
-    setLabel("Task(s): ")
     setTasks(prev => {
-      return [...prev, task]
+      const newTasks = [...prev, task]
+      localStorage.setItem("tasks", JSON.stringify(newTasks))
+      return newTasks
     })
   }
   const handleRemove = index => {
     const newTasks = [...tasks]
     newTasks.splice(index, 1)
-    console.log(tasks.length);
-    if (newTasks.length === 0) 
-      setLabel("Nothing to do")
+    localStorage.setItem("tasks", JSON.stringify(newTasks))
     return setTasks(newTasks)
   }
   useEffect(() => {
     console.log(tasks);
   }, [tasks])
+
   return (
     <div className="App">
-      <h1>Todo App</h1>
-      <div>
-        <input 
-          value = {task}
-          onChange = {(e) => setTask(e.target.value)}
-          ref = {inputRef}
-          className = "input-box"
-          placeholder = "Add task here..."
-        />
-        <button 
-          onClick={handleAdd}
-          className = "submit-btn"
-        >
-          +
-        </button>
-        <ol className='list'>
-          <p className="label">{label}</p>
+      <div className="app-container">
+        <h1>Todo App</h1>
+        <hr></hr>
+        <div className='data-input-box'>
+          <input 
+            value = {task}
+            onChange = {(e) => setTask(e.target.value)}
+            ref = {inputRef}
+            className = "input-box"
+            placeholder = "Add task here..."
+          />
+          <button 
+            onClick={handleAdd}
+            className = "submit-btn"
+          >
+            <i className="fa-solid fa-plus"></i>
+          </button>
+        </div>
+        <ul className='list'>
           {tasks.map((task, index) => (
             <React.Fragment key = {index}>
-                <li className = "list-item">{task}</li>
-                <button 
-                  onClick={() => handleRemove(index)}
-                  className = "remove-btn"
-                > 
-                  &times;
-                </button>
+                <li className = "list-item">
+                  <div className='content-block'>
+                    {task}
+                  </div>
+                  <div className='edit-block'>
+                    <button 
+                      onClick={() => handleRemove(index)}
+                      className = "remove-btn"
+                    > 
+                      <i className="fa-solid fa-xmark"></i>
+                    </button>
+                  </div>
+                </li>
               </React.Fragment>
           ))}
-        </ol>
+        </ul>
       </div>
     </div>
   );
 }
 
-export default App;
+export default App; 
